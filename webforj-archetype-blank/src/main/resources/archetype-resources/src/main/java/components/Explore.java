4c25385fc27e61e75c924fa0e07d1fa1b45b10b5
Hyyan Abo Fakher <hyyanaf@gmail.com>
2026-04-17 12:37:50 +0200

    refactor: rewrite blank archetype

--- webforj-archetype-blank/src/main/resources/archetype-resources/src/main/java/components/Explore.java
+++ webforj-archetype-blank/src/main/resources/archetype-resources/src/main/java/components/Explore.java
@@ -3,31 +3,46 @@
 #set( $symbol_escape = '\' )
 package ${package}.components;
 
+import com.webforj.Page;
 import com.webforj.component.Composite;
-import com.webforj.component.html.elements.Anchor;
+import com.webforj.component.button.Button;
+import com.webforj.component.button.ButtonTheme;
 import com.webforj.component.html.elements.H3;
-import com.webforj.component.html.elements.Img;
+import com.webforj.component.icons.Icon;
+import com.webforj.component.icons.TablerIcon;
 import com.webforj.component.layout.flexlayout.FlexAlignment;
 import com.webforj.component.layout.flexlayout.FlexDirection;
 import com.webforj.component.layout.flexlayout.FlexLayout;
 
 public class Explore extends Composite<FlexLayout> {
+  private static final String DOCS_URL = "https://docs.webforj.com/docs/components/overview";
+
   private FlexLayout self = getBoundComponent();
 
   public Explore() {
     self.addClassName("explore-component");
     self.setStyle("margin", "1em auto");
+    self.setStyle("text-align", "center");
     self.setDirection(FlexDirection.COLUMN);
     self.setAlignment(FlexAlignment.CENTER);
-    self.setMaxWidth(300);
-    self.setSpacing(".3em");
+    self.setMaxWidth(400);
+    self.setSpacing(".75em");
 
-    Img img = new Img("ws://explore.svg", "Explore");
-    img.setMaxWidth(250);
+    Icon icon = TablerIcon.create("sparkles");
+    icon.setStyle("font-size", "3rem");
 
-    Anchor anchor = new Anchor("https://docs.webforj.com/docs/components/overview", "Start with webforJ Components");
-    anchor.setTarget("_blank");
+    FlexLayout badge = FlexLayout.create(icon).align().center().justify().center().build();
+    badge.setStyle("width", "6rem");
+    badge.setStyle("height", "6rem");
+    badge.setStyle("border-radius", "50%");
+    badge.setStyle("background", "var(--dwc-color-primary-alt)");
+    badge.setStyle("color", "var(--dwc-color-on-primary-text-alt)");
 
-    self.add(img, new H3("Ready to create an awesome UI?"), anchor);
+    Button cta = new Button("Explore components")
+        .setSuffixComponent(TablerIcon.create("arrow-narrow-right"))
+        .setTheme(ButtonTheme.PRIMARY);
+    cta.onClick(ev -> Page.getCurrent().open(DOCS_URL, "_blank"));
+
+    self.add(badge, new H3("Ready to create an awesome UI?"), cta);
   }
 }
